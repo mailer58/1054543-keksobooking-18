d20ac206c4472f85de11cdef85b05440f829cfc4
mailer58 <50555861+mailer58@users.noreply.github.com>
2019-09-27 22:42:00 +0300

    Fix according to comments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var NOTICES_QUANTITY = 8;
-var notices = [];
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
 var offerType = ['palace', 'flat', 'house', 'bungalo'];
 var times = ['12:00', '13:00', '14:00'];
 var features = ['wifi', 'dishwasher', 'parking',
@@ -11,7 +12,7 @@ var features = ['wifi', 'dishwasher', 'parking',
 var pinDestination = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin')
   .content.querySelector('.map__pin');
-var showMap = document.querySelector('.map');
+var bookingMap = document.querySelector('.map');
 
 function random(arr) {
   return Math.floor(Math.random() * arr.length);
@@ -31,7 +32,7 @@ function getRandomArray(arr) {
   for (var i = 0; i < arr.length; i++) {
     copyArray[i] = arr[i];
   }
-  var randomArrayLength = (random(arr) !== 0) ? random(arr) : 1;
+  var randomArrayLength = getRandomIntegerFromDiapason(1, arr.length);
   // generation of random array:
   for (i = 0; i < randomArrayLength; i++) {
     var randomIndex = random(copyArray);
@@ -40,8 +41,6 @@ function getRandomArray(arr) {
   }
   return randomArray;
 }
-
-showMap.classList.remove('map--faded');
 
 // array of links:
 function getLinksArray(min, max, linkBegining, imgExtension) {
@@ -53,9 +52,10 @@ function getLinksArray(min, max, linkBegining, imgExtension) {
   return randomArray;
 }
 
-function generateNotices(arr) {
-  for (var i = 0; i < NOTICES_QUANTITY; i++) {
-    arr[i] = {
+function generateNotices(noticesQuantity) {
+  var notices = [];
+  for (var i = 0; i < noticesQuantity; i++) {
+    notices[i] = {
       author: {
         avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
@@ -63,14 +63,14 @@ function generateNotices(arr) {
         title: 'Заголовок объявления',
         address: getRandomNumber(1000) + ',' + getRandomNumber(10000),
         price: getRandomNumber(10000),
-        type: offerType[random(offerType)],
+        type: offerType[getRandomNumber(offerType.length)],
         rooms: getRandomNumber(10),
         guests: getRandomNumber(10),
-        checkin: times[random(times)],
-        checkout: times[random(times)],
+        checkin: times[getRandomNumber(times.length)],
+        checkout: times[getRandomNumber(times.length)],
         features: getRandomArray(features),
         description: 'Описание',
-        photos: getLinksArray(1, 3, 'http://o0.github.io/assets/images/tokyo/hotel', '.jpg')
+        photos: getLinksArray(1, 10, 'http://o0.github.io/assets/images/tokyo/hotel', '.jpg')
       },
       location: {
         x: getRandomNumber(1201),
@@ -78,21 +78,21 @@ function generateNotices(arr) {
       }
     };
   }
+  return notices;
 }
 
 function getNewElements(numberOfCopy, template, arr, destination) {
   var fragment = document.createDocumentFragment();
-  var pinWidth = 50;
-  var pinHeight = 70;
   for (var i = 0; i < numberOfCopy; i++) {
     var newElement = template.cloneNode(true);
     fragment.appendChild(newElement);
-    newElement.style.left = arr[i].location.x - pinWidth / 2 + 'px';
-    newElement.style.top = arr[i].location.y - pinHeight + 'px';
+    newElement.style.left = arr[i].location.x - PIN_WIDTH / 2 + 'px';
+    newElement.style.top = arr[i].location.y - PIN_HEIGHT + 'px';
     newElement.querySelector('img').src = arr[i].author.avatar;
     newElement.querySelector('img').alt = arr[i].offer.title;
   }
   destination.appendChild(fragment);
 }
-generateNotices(notices);
+bookingMap.classList.remove('map--faded');
+var notices = generateNotices(NOTICES_QUANTITY);
 getNewElements(NOTICES_QUANTITY, pinTemplate, notices, pinDestination);
