debc7dfd928da193a9d8778f39954eb42802cd9d
mailer58 <50555861+mailer58@users.noreply.github.com>
2019-11-10 22:01:40 +0300

    Add residual tasks

--- js/backend.js
+++ js/backend.js
@@ -1,30 +1,52 @@
 'use strict';
 
-(function () {
+(function() {
   window.backend = {
+    //download data:
     load: function load(onDownload, onDownloadError) {
       var URL = 'https://js.dump.academy/keksobooking/data';
       var xhr = new XMLHttpRequest();
       xhr.responseType = 'json';
-      xhr.addEventListener('load', function () {
+      xhr.addEventListener('load', function() {
         if (xhr.status === 200) {
           onDownload(xhr.response);
         } else {
           onDownloadError();
         }
       });
-      xhr.addEventListener('error', function () {
+      xhr.addEventListener('error', function() {
         onDownloadError();
       });
-      xhr.addEventListener('timeout', function () {
+      xhr.addEventListener('timeout', function() {
         onDownloadError();
       });
 
-      xhr.timeout = 15000; // 10s
+      xhr.timeout = 15000;
 
       xhr.open('GET', URL);
       xhr.send();
-    }
+    }, // upload data:
+    save: function save(data, onUpload, onUploadError) {
+      var URL = 'https://js.dump.academy/keksobooking';
+      var xhr = new XMLHttpRequest();
+      xhr.addEventListener('load', function() {
+        if (xhr.status === 200) {
+          console.log('Ok');
+          onUpload(data);
+        } else {
+          onUploadError();
+        }
+      });
+      xhr.addEventListener('error', function() {
+        onUploadError();
+      });
+      xhr.addEventListener('timeout', function() {
+        onUploadError();
+      });
 
+      xhr.timeout = 15000; // 10s
+      xhr.open('POST', URL);
+      xhr.send(new FormData(data));
+    }
   };
 })();
--- js/util.js
+++ js/util.js
@@ -1,45 +1,59 @@
 'use strict';
 
-(function () {
-  window.util = {
-    getRandomNumber: function (number) {
-      return Math.floor(Math.random() * number);
-    },
-    getRandomIntegerFromDiapason: function getRandomIntegerFromDiapason(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-    getRandomArray: function (arr) {
-      var randomArray = [];
-      var copyArray = [];
-      for (var i = 0; i < arr.length; i++) {
-        copyArray[i] = arr[i];
+(function() {
+    var DEBOUNCE_INTERVAL = 500;
+    window.util = {
+      getRandomNumber: function(number) {
+        return Math.floor(Math.random() * number);
+      },
+      getRandomIntegerFromDiapason: function getRandomIntegerFromDiapason(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      },
+      getRandomArray: function(arr) {
+        var randomArray = [];
+        var copyArray = [];
+        for (var i = 0; i < arr.length; i++) {
+          copyArray[i] = arr[i];
+        }
+        var randomArrayLength = this.getRandomIntegerFromDiapason(1, arr.length);
+        // generation of random array:
+        for (i = 0; i < randomArrayLength; i++) {
+          var randomIndex = this.getRandomNumber(copyArray.length);
+          randomArray[i] = copyArray[randomIndex];
+          copyArray.splice(randomIndex, 1);
+        }
+        return randomArray;
+      },
+      // array of links:
+      getLinksArray: function(min, max, linkBegining, imgExtension) {
+        var randomArray = [];
+        var randomArrayLength = this.getRandomIntegerFromDiapason(min, max);
+        for (var i = 0; i < randomArrayLength; i++) {
+          randomArray[i] = linkBegining + (i + 1) + imgExtension;
+        }
+        return randomArray;
+      },
+      onCloseButtonClick: function(evt) {
+        evt.target.closest('.popup').removeEventListener('click', window.util.onCloseButtonClick);
+        evt.target.closest('.popup').remove();
+      },
+      getFormattedAddress: function(left, width, top, height) {
+        var leftPosition = Math.floor(parseInt(left, 10) + width);
+        var topPosition = Math.floor(parseInt(top, 10) + height);
+        return leftPosition + ', ' + topPosition;
+      },
+      debounce: function(func) {
+        var lastTimeout = null;
+        return function() {
+          var parameters = arguments;
+          if (lastTimeout) {
+            window.clearTimeout(lastTimeout);
+          }
+          lastTimeout = window.setTimeout(function() {
+            func.apply(null, parameters);
+          }, DEBOUNCE_INTERVAL);
+        }
       }
-      var randomArrayLength = this.getRandomIntegerFromDiapason(1, arr.length);
-      // generation of random array:
-      for (i = 0; i < randomArrayLength; i++) {
-        var randomIndex = this.getRandomNumber(copyArray.length);
-        randomArray[i] = copyArray[randomIndex];
-        copyArray.splice(randomIndex, 1);
-      }
-      return randomArray;
-    },
-    // array of links:
-    getLinksArray: function (min, max, linkBegining, imgExtension) {
-      var randomArray = [];
-      var randomArrayLength = this.getRandomIntegerFromDiapason(min, max);
-      for (var i = 0; i < randomArrayLength; i++) {
-        randomArray[i] = linkBegining + (i + 1) + imgExtension;
-      }
-      return randomArray;
-    },
-    onCloseButtonClick: function (evt) {
-      evt.target.closest('.popup').style.display = 'none';
-    },
-    getFormattedAddress: function (left, width, top, height) {
-      var leftPosition = Math.floor(parseInt(left, 10) + width);
-      var topPosition = Math.floor(parseInt(top, 10) + height);
-      return leftPosition + ', ' + topPosition;
     }
-  };
 
-})();
+    })();
